5f4b5c33ee1f978a01ffb8b3fab929b23e463110
YanaMikulskaya <miklushka26@gmail.com>
2026-05-20 17:36:55 +0300

    feat: add complete routing configuration

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-
+import { Outlet } from 'react-router';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { Main } from './Main';
-//import { Outlet } from 'react-router';
-type TitleProps = {
-  children: React.ReactNode;
-};
 
-export function Layout({ children }: TitleProps): React.ReactElement {
+export function Layout(): React.ReactElement {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="mx-auto w-full max-w-[1180px] flex-1 px-[30px] flex flex-col">
         <Main>
-          {children}
-          {/*<Outlet />*/}
+          <Outlet />
         </Main>
         <Footer />
       </div>
